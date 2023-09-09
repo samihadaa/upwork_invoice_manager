c204ffa1691fe5deb26213b3c57d8c561d75444a
Sami Khiari <samihada.madaraa@gmail.com>
2023-09-09 22:10:19 +0200

    Styling the header of the app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
 <div>
-  <div class="app flex flex-column">
+  <div v-if="!isMobile" class="app flex flex-column">
     <Navigation />
     <div class="app-content flex flex-column">
        <router-view />
     </div>
+  </div>
+  <div v-else class="mobile-message flex flex-column">
+    <h2>Sorry, this app is not supported on mobile devices</h2>
+    <p>To use this app, please use a computer or a tablet</p>
   </div>
 </div>
   
@@ -53,6 +57,22 @@ export default{
   min-height: 100vh;
   @media(min-width: 900px){
     flex-direction: row !important;
+  }
+  .app-content{
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+.mobile-message {
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color:#fff;
+  p{
+    margin-top: 16px;
   }
 }
 button,
